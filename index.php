921a234b6c3d413d5318f34bfe97895a8f2b19a9
biliarxx <biliarxx@yandex.ru>
2022-11-20 19:12:53 +0700

    form processing

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
     $title = "Главная страница";
     require_once "blocks/header.php";
 ?>
+<div class="container mt-2">
     <h1>Главная страница</h1>
+    <form action="result.php" method="post">
+        <input type="text" name="name" placeholder="Введите имя" class="form-control"><br>
+        <input type="text" name="surname" placeholder="Введите фамилию" class="form-control"><br>
+        <input type="text" name="phoneNumber" placeholder="Введите номер телефона" class="form-control"><br>
+        <input type="submit" value="Сохранить" class="btn btn-success">
+    </form>
+</div>
+
 <?php
     require_once "blocks/footer.php";
 
--- result.php
+++ result.php
@@ -1,22 +1,10 @@
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>База пользователей</title>
-</head>
-<body>
-<header>
-    <a href="index.php">Главная</a> |
-    <a href="result.php" >База пользователей</a>
-</header>
-<?php
 
+<?php
+    $title = "База пользоваелей";
+    require_once "blocks/header.php";
 ?>
-<h1>База пользователей</h1>
-<footer>Все права защищены &copy; 2023</footer>
-</body>
-</html>
+    <h1>База пользователей</h1>
+<?php
+    print_r($_POST);
+    require_once "blocks/footer.php";
 
