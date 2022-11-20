8adb6d748e5507dbb71d4fc805350e0090ecb38d
biliarxx <biliarxx@yandex.ru>
2022-11-20 18:31:57 +0700

    connecting files

---
+++ blocks/footer.php
@@ -0,0 +1,3 @@
+<footer>Все права защищены &copy; 2023</footer>
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>База пользователей</title>
+</head>
+<body>
+<header>
+    <a href="index.php">Главная</a> |
+    <a href="result.php" >База пользователей</a>
+</header>
+<?php
+
+?>
+<h1>База пользователей</h1>
+<footer>Все права защищены &copy; 2023</footer>
+</body>
+</html>
+
