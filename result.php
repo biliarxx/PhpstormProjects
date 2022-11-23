d7e3a28cdb8128a79bece393c93ea85c9da35e72
biliarxx <biliarxx@yandex.ru>
2022-11-23 22:45:33 +0700

    MySQL added

--- result.php
+++ result.php
@@ -5,6 +5,30 @@
 ?>
     <h1>База пользователей</h1>
 <?php
-    print_r($_POST);
-    require_once "blocks/footer.php";
 
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $phoneNumber = $_POST['phoneNumber'];
+
+    $mysql = new mysqli("localhost", "root", "");
+    $mysql-> query("SET NAMEs 'utf8'");
+
+    if ($mysql->connect_error) {
+        echo "Error: ".$mysql->connect_error;
+    } else {
+        $mysql-> query("CREATE TABLE `users` (
+  `id` int(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `phoneNumber` int(11) NOT NULL
+)");
+    }
+
+    $mysql->query("INSERT INTO `users`(`name` , `surname`, `phoneNumber`)
+    VALUES ('$name', '$surname', '$phoneNumber')");
+
+    $mysql-> close();
+
+
+    require_once "blocks/footer.php";
+?>
