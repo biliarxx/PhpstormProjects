ee4dbf4ad98018d96805758016c7125f06bdd4c2
biliarxx <biliarxx@yandex.ru>
2022-12-12 22:14:28 +0700

    try catch added

--- result.php
+++ result.php
@@ -6,28 +6,36 @@
     <h1>База пользователей</h1>
 <?php
 
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $phoneNumber = $_POST['phoneNumber'];
+    $name = "qwe";//$_POST['name'];
+    $surname = "rty";//$_POST['surname'];
+    $phoneNumber = 123456;//$_POST['phoneNumber'];
 
-    $mysql = new mysqli("localhost", "root", "");
+    $mysql = new mysqli("localhost", "root", "", $dbname = "dbname");
     $mysql-> query("SET NAMEs 'utf8'");
 
-    if ($mysql->connect_error) {
-        echo "Error: ".$mysql->connect_error;
-    } else {
-        $mysql-> query("CREATE TABLE `users` (
-  `id` int(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+try {
+    $mysql->query("CREATE DATABASE IF NOT EXISTS `$dbname`");
+    $mysql-> query("CREATE TABLE `users` (
+  `id` int(20) NOT NULL  AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
-  `phoneNumber` int(11) NOT NULL
+  `phoneNumber` int(11) NOT NULL,
+  PRIMARY KEY (id);
 )");
-    }
 
-    $mysql->query("INSERT INTO `users`(`name` , `surname`, `phoneNumber`)
-    VALUES ('$name', '$surname', '$phoneNumber')");
+    $mysql->query("INSERT INTO `users`(`name`, `surname`, `phoneNumber`) VALUES ($name,$surname,$phoneNumber");
+    $result = $mysql-> query("SELECT * FROM `$dbname` WHERE 1")->fetch_all();
 
-    $mysql-> close();
+} catch (PDOException $e) {
+    throw new Exception($e->getMessage());
+}
+$mysql-> close();
+
+//    if ($mysql->connect_error) {
+//        echo "Error: ".$mysql->connect_error;
+//    } else {
+
+
 
 
     require_once "blocks/footer.php";
