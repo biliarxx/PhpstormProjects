7fc4ad6dd7a2254c4db0db70488e3a883c52614e
biliarxx <biliarxx@yandex.ru>
2023-01-30 21:25:40 +0700

    create table and print_r added

--- result.php
+++ result.php
@@ -1,75 +1,58 @@
 
 <?php
-    $title = "База пользоваелей";
-    require_once "blocks/header.php";
+$title = "База пользоваелей";
+require_once "blocks/header.php";
 ?>
-    <h1>База пользователей</h1>
+<h1>База пользователей</h1>
 <?php
 
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $phoneNumber = $_POST['phoneNumber'];
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$phoneNumber = $_POST['phoneNumber'];
 
 try {
     $connection = new mysqli('localhost','root','');
     if ($connection->connect_errno){
         throw new Exception($connection->connect_error);
     } else {
-        echo "22222<br>";
-//        $sql = "CREATE DATABASE testdb3";
-        $sqlDataBase = "CREATE DATABASE dbname";
-      }
+        $sqlDataBase = "CREATE DATABASE IF NOT EXISTS dbname";
+    }
     if ($connection->query($sqlDataBase)) {
-            echo "База данных успешно создана";
-            $sqlTable = "CREATE TABLE users (id int(20) NOT NULL  AUTO_INCREMENT, 
-             name varchar(255) NOT NULL, 
+        $connectionTable = new mysqli('localhost','root','', 'dbname');
+        $sqlCreateTable = "CREATE TABLE IF NOT EXISTS users (id int(20) NOT NULL  AUTO_INCREMENT, 
+             user_name varchar(255) NOT NULL, 
              surname varchar(255) NOT NULL, 
              phoneNumber int(11) NOT NULL, 
-              PRIMARY KEY (id);)";
-            $sqlTable = "INSERT INTO `users`(`name`, `surname`, `phoneNumber`) VALUES ($name,$surname,$phoneNumber)";
-            $results = $connection -> query("SELECT * FROM dbname")->fetch_all(MYSQLI_ASSOC);
-            foreach ($results as $result) {
+              PRIMARY KEY (id))";
+        if ($connectionTable->query($sqlCreateTable)){
+            $sqlInsertTable = "INSERT INTO `users`(`name`, `surname`, `phoneNumber`) VALUES ('".$name."','".$surname."','".$phoneNumber."')";
+            if($connectionTable->query($sqlInsertTable)){
+                $results = $connectionTable -> query("SELECT * FROM users")->fetch_all(MYSQLI_ASSOC);
                 echo "<pre>";
-                print_r($result);
-                echo "result name";
+                print_r($results);
                 echo "</pre>";
-            } if ($connection->connect_error) {
-                echo "Ошибка: " . $connection->error;
             }
         }
+    }
 } catch (Exception $e) {
     echo $e->getMessage();
 }
 $connection->close();
-//try {
-//    $mysql = new mysqli("localhost", "root", "", $dbname = "dbname");
-//    $mysql-> query("SET NAMEs 'utf8'");
-//
-//    $mysql->query("CREATE DATABASE IF NOT EXISTS`$dbname`");
-//    $mysql-> query("CREATE TABLE IF NOT EXISTS`users` (
-//  `id` int(20) NOT NULL  AUTO_INCREMENT,
-//  `name` varchar(255) NOT NULL,
-//  `surname` varchar(255) NOT NULL,
-//  `phoneNumber` int(11) NOT NULL,
-//  PRIMARY KEY (id);
-//)");
-//
-//    $mysql->query("INSERT INTO `users`(`name`, `surname`, `phoneNumber`) VALUES ($name,$surname,$phoneNumber");
-//    $results = $mysql-> query("SELECT * FROM `$dbname`")->fetch_all(MYSQLI_ASSOC);
-//    foreach ($results as $result) {
-//            echo "<pre>";
-//        print_r($result);
-//        echo "result name";
-//            echo "</pre>";
-//    }
-//    $mysql-> close();
-//} catch (PDOException $e) {
-//    throw new Exception($e->getMessage());
-//}
+$connectionTable->close();
 
-//    if ($mysql->connect_error) {
-//        echo "Error: ".$mysql->connect_error;
-//    } else {
-
-    require_once "blocks/footer.php";
+require_once "blocks/footer.php";
 ?>
+<table>
+    <tr>
+        <th>Company</th><th>Q1</th><th>Q2</th><th>Q3</th><th>Q4</th>
+    </tr>
+    <tr>
+        <td>Microsoft</td><td>20.3</td><td>30.5</td><td>23.5</td><td>40.3</td>
+    </tr>
+    <tr>
+        <td>Google</td><td>50.2</td><td>40.63</td><td>45.23</td><td>39.3</td>
+    </tr>
+    <tr>
+        <td>Apple</td><td>25.4</td><td>30.2</td><td>33.3</td><td>36.7</td>
+    </tr>
+</table>
